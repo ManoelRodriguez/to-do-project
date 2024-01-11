ea296e32c984bb3b9258b81abc017e7f99e5b3dd
Manoel Rodriguez <manoel.rodriguez98@gmail.com>
2024-01-11 15:39:23 -0300

    Implementando Função de Adicionar Tarefa

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const button = document.querySelector('.button-add-task');
+const input = document.querySelector('.input-task');
+const listaCompleta = document.querySelector('.list-tasks')
+
+let minhaListaDeItens = [];
+
+function adicionarNovaTarefa(){
+    minhaListaDeItens.push(input.value);
+
+    mostrarTarefas();
+}
+
+function mostrarTarefas(){
+
+    let novaLi = '';
+
+    minhaListaDeItens.forEach((tarefa) => {
+
+        novaLi = novaLi + `
+            <li class="task">
+                <img src="./img/checked.png" alt="check-na-tarefa">
+                <p>${tarefa}</p>
+                <img src="./img/trash.png" alt="excluir-tarefa">
+            </li>
+
+        
+        `
+    })
+
+    listaCompleta.innerHTML = novaLi;
+}
+
+button.addEventListener('click', adicionarNovaTarefa);
